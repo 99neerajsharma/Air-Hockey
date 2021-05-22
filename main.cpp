4b77c840af74d6e7e614db813f4674f481d30c2f
Saurabh Bansal <saurabh18213@gmail.com>
2021-05-22 23:30:50 +0530

    Texture code Added

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ using namespace glm;
 #include <utils/controls.hpp>
 #include <utils/objloader.hpp>
 #include <utils/shader.hpp>
+#include <utils/texture.hpp>
 
 int main(){
     // Initialise GLFW
@@ -36,7 +37,7 @@ int main(){
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Robotic Arm", NULL, NULL);
+	window = glfwCreateWindow( 1024, 768, "Air Hockey", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
 		getchar();
@@ -64,7 +65,7 @@ int main(){
     glfwSetCursorPos(window, 1024/2, 768/2);
 
 	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 
 	// Enable depth test
 	glEnable(GL_DEPTH_TEST);
@@ -79,10 +80,15 @@ int main(){
 	glBindVertexArray(VertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader" );
+	GLuint programID = LoadShaders( "shaders/ColorVertexShader.vertexshader", "shaders/ColorFragmentShader.fragmentshader" );
+	GLuint programIDTexture = LoadShaders( "shaders/TextureVertexShader.vertexshader", "shaders/TextureFragmentShader.fragmentshader" );
+	GLuint Texture = loadBMP_custom("textures/Table.bmp");
+	GLuint TextureID  = glGetUniformLocation(programIDTexture, "myTextureSampler");
+
 
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+	GLuint MatrixIDTexture = glGetUniformLocation(programIDTexture, "MVP");
 
     std::vector<glm::vec3> table_vertices;
 	std::vector<glm::vec2> table_uvs;
@@ -113,9 +119,9 @@ int main(){
 
     GLfloat puck_color_buffer_data[puck_vertices_size*3];
 	for (int v = 0; v < puck_vertices_size ; v++){
-		puck_color_buffer_data[3*v+0] = 1.0;
-		puck_color_buffer_data[3*v+1] = 1.0;
-		puck_color_buffer_data[3*v+2] = 1.0;
+		puck_color_buffer_data[3*v+0] = 0.2;
+		puck_color_buffer_data[3*v+1] = 0.2;
+		puck_color_buffer_data[3*v+2] = 0.2;
 	}
 
     GLfloat striker_neg_color_buffer_data[striker_vertices_size*3];
@@ -132,10 +138,22 @@ int main(){
 		striker_pos_color_buffer_data[3*v+2] = 0.09;
 	}
 
+	GLfloat table_parts_color_buffer_data[table_parts_vertices_size*3];
+	for (int v = 0; v < table_parts_vertices_size ; v++){
+		table_parts_color_buffer_data[3*v+0] = 1.0;
+		table_parts_color_buffer_data[3*v+1] = 1.0;
+		table_parts_color_buffer_data[3*v+2] = 1.0;
+	}
+
     GLuint table_vertex_buffer;
 	glGenBuffers(1, &table_vertex_buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, table_vertex_buffer);
 	glBufferData(GL_ARRAY_BUFFER, table_vertices.size() * sizeof(glm::vec3), &table_vertices[0], GL_STATIC_DRAW);
+
+	GLuint table_uvs_vertex_buffer;
+	glGenBuffers(1, &table_uvs_vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, table_uvs_vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, table_uvs.size() * sizeof(glm::vec2), &table_uvs[0], GL_STATIC_DRAW);
 
     GLuint table_parts_vertex_buffer;
 	glGenBuffers(1, &table_parts_vertex_buffer);
@@ -174,13 +192,18 @@ int main(){
 	glBindBuffer(GL_ARRAY_BUFFER, striker_pos_color_buffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(striker_pos_color_buffer_data), striker_pos_color_buffer_data, GL_STATIC_DRAW);
     
+	GLuint table_parts_color_buffer;
+	glGenBuffers(1, &table_parts_color_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, table_parts_color_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(table_parts_color_buffer_data), table_parts_color_buffer_data, GL_STATIC_DRAW);
+    
     do{
 
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Use our shader
-		glUseProgram(programID);
+		glUseProgram(programIDTexture);
 
 		// Compute the MVP matrix from keyboard and mouse input
 		computeMatricesFromInputs();
@@ -192,13 +215,30 @@ int main(){
 
 		// Send our transformation to the currently bound shader, 
 		// in the "MVP" uniform
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+		glUniformMatrix4fv(MatrixIDTexture, 1, GL_FALSE, &MVP[0][0]);
+
+		// Bind our texture in Texture Unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, Texture);
+		// Set our "myTextureSampler" sampler to use Texture Unit 0
+		glUniform1i(TextureID, 0);
 
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, table_vertex_buffer);
 		glVertexAttribPointer(
 			0,                  // attribute
 			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, table_uvs_vertex_buffer);
+		glVertexAttribPointer(
+			1,                  // attribute
+			2,                  // size
 			GL_FLOAT,           // type
 			GL_FALSE,           // normalized?
 			0,                  // stride
@@ -218,11 +258,27 @@ int main(){
 		// glPushMatrix();
 		glDrawArrays(GL_TRIANGLES, 0, table_vertices.size() );
 		// glPopMatrix();
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+		
+		glUseProgram(programID);
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
         glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, table_parts_vertex_buffer);
 		glVertexAttribPointer(
 			0,                  // attribute
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, table_parts_color_buffer);
+		glVertexAttribPointer(
+			1,                  // attribute
 			3,                  // size
 			GL_FLOAT,           // type
 			GL_FALSE,           // normalized?
@@ -243,6 +299,8 @@ int main(){
 		// glPushMatrix();
 		glDrawArrays(GL_TRIANGLES, 0, table_parts_vertices.size() );
 		// glPopMatrix();
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
 
         glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, puck_vertex_buffer);
@@ -268,6 +326,8 @@ int main(){
 		// glPushMatrix();
 		glDrawArrays(GL_TRIANGLES, 0, puck_vertices.size() );
 		// glPopMatrix();
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
 
         glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, striker_neg_vertex_buffer);
@@ -293,6 +353,9 @@ int main(){
 		// glPushMatrix();
 		glDrawArrays(GL_TRIANGLES, 0, striker_neg_vertices.size() );
 		// glPopMatrix();
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
 
         glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, striker_pos_vertex_buffer);
